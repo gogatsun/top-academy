368d92937c978e966e956cd43d688ceb44a53c05
Katherina Kosterina <139794238+gogatsun@users.noreply.github.com>
2023-10-22 08:36:45 +0300

    Update RdbOrderService.java

--- Java213_КостеринаЕС_Задание22ПродуктыЗаказы/ProductsAndOrdersApiApp/src/main/java/org/top/productsandordersapiapp/rdb/service/RdbOrderService.java
+++ Java213_КостеринаЕС_Задание22ПродуктыЗаказы/ProductsAndOrdersApiApp/src/main/java/org/top/productsandordersapiapp/rdb/service/RdbOrderService.java
@@ -51,16 +51,12 @@ public class RdbOrderService implements OrderService {
     }
 
     @Override
-    public Optional<Order> update(Order order) {
-        Optional<Order> updated = orderRepository.findById(order.getId());
+    public Optional<Order> update(Integer id, String description) {
+        Optional<Order> updated = getById(id);
         if (updated.isPresent()) {
-            Order newOrder = new Order();
-            newOrder.setId(updated.get().getId());
-            newOrder.setDescription(order.getDescription());
-            newOrder.setClient(updated.get().getClient());
-            return Optional.of(orderRepository.save(newOrder));
-
+            updated.get().setDescription(description);
+            orderRepository.save(updated.get());
         }
-        return Optional.empty();
+        return updated;
     }
 }
