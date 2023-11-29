b4ae5eb0e36b7b9aa3d9ecd2bcf3a039eb668643
Katherina Kosterina <139794238+gogatsun@users.noreply.github.com>
2023-11-29 23:39:53 +0300

    Add files via upload

---
+++ BookStoreApplication/src/main/java/com/example/bookstoreapplication/service/AuthorService.java
@@ -0,0 +1,33 @@
+package com.example.bookstoreapplication.service;
+
+import com.example.bookstoreapplication.entity.Author;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+/**Методы:<br>
+ * <i>add()</i> - добавить автора<br>
+ * <i>findById</i> - получить по ID<br>
+ * <i>findAll</i> - получить все<br>
+ * <i>update</i> - изменить<br>
+ * <i>findByName</i> - получить по имени*/
+@Service
+public interface AuthorService {
+
+    // добавить
+    Optional<Author> add(Author author);
+
+    // получить по ID
+    Optional<Author> findById(Long id);
+
+    // получить все
+    List<Author> findAll();
+
+    //изменить
+    Optional<Author> update(Author author);
+
+    //получить по имени
+    List<Author> findByName(String name);
+
+}
